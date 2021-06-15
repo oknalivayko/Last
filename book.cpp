395eb7c359da7cb633356867997cadd3e530699e
Asya Khabibova <asikan2014@ya.ru>
2021-06-15 12:55:00 +0300

    generate

--- book.cpp
+++ book.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<stdlib.h>
 #include <fstream>
+#include<string>
+#include <ctime>
 
 using namespace std;
 
@@ -82,6 +84,59 @@ void delSite(int c) {
 	email[c] = "";
 	password[c] = "";
 }
+void Generate() {
+    srand(static_cast<unsigned int>(time(0))); // устанавливаем значение системных часов в качестве стартового числа
+    int level;
+    int length;
+    do{
+        cout<<"1-Weak   ****  2-Average  ****  3-Strong"<<endl;
+        cout << "Please enter password complexity. " << endl;
+        cin>> level;
+
+        if( level<1 || level>3)
+            cout<<"Invalid Complexity Entered. Please Re-Enter"<<endl;
+    }
+    while( level<1 || level>3 );
+
+    if (level == 1) length = 8;
+    else if(level == 2) length = 14;
+    else length = 20;
+
+    char pass[21] = "";
+
+    int i;
+    for(i=0; i<length; i++ ){
+        if( level == 1 ){
+            if( rand()%3 == 0 )
+                pass[i] = 86 + rand()%34;
+            else
+                pass[i] = 46 + rand()%10;
+        }
+        else if( level == 2 ){
+            if( rand()%4 == 0 )
+                pass[i] = 86 + rand()%34;
+            else if( rand()%3 == 1 )
+                pass[i] = 53 + rand()%26;
+            else
+                pass[i] = 46 + rand()%10;
+        }
+        else if( level == 3 ){
+            if( rand()%5 == 0 )
+                pass[i] = 86 + rand()%34;
+            else if( rand()%4 == 1 )
+                pass[i] = 53 + rand()%26;
+            else if( rand()%4 == 2 )
+                pass[i] = 32 + rand()%15;
+            else
+                pass[i] = 46 + rand()%10;
+        }
+    }
+    for(int i=0; i<length; i++){
+        cout<<pass[i];
+    }
+    cout << endl;
+}
+
 int find() {
 	if (k < 0)
 		return -1;
@@ -100,23 +155,17 @@ int find() {
 int main() {
 	char op;
 	do {
-		ifstream in("sitename.txt");
-		for (int i = 0; i < 50; i++) {
-			in >> sitename[i];
-			in >> number[i];
-			in >> email[i];
-			in >> password[i];
-		}
-
-		in.close();
+		
 		system("cls");	
 		cout << "1. Add website" << endl;
-		cout << "2. List of all websites" << endl;
-		cout << "3. Search" << endl;
-		cout << "4. Update" << endl;
-		cout << "5. Delete" << endl;
-		cout << "6. Save" << endl;
-		cout << "7. Exit" << endl << endl;
+        cout << "2. Generate Password" << endl;
+		cout << "3. List of all websites" << endl;
+		cout << "4. Search" << endl;
+		cout << "5. Update" << endl;
+		cout << "6. Delete" << endl;
+		cout << "7. Save" << endl;
+		cout << "8. Open" << endl;
+		cout << "9. Exit" << endl << endl;
 		cout << "Choose the option:";
 		cin >> op;
 
@@ -128,12 +177,21 @@ int main() {
 			system("pause");
 			break;
 		}
-		case '2':
+            case '2': {
+                cout << "   ****      PASSWORD  GENERATOR     ***      " << endl;
+                cout << "   Welcome to the Password Generator.     " << endl;
+                cout << "   You can generate password and choose the difficulty level for your password." << endl << endl;
+                Generate();
+				system("pause");
+                break;
+            }
+		case '3':
 		{
 			printAll();
+			system("pause");
 			break;
 		}
-		case '3':
+		case '4':
 		{
 			int c = find();
 			if (c > -1) {
@@ -141,7 +199,7 @@ int main() {
 			}
 			break;
 		}
-		case '4':
+		case '5':
 		{
 			int c = find();
 			if (c > -1) {
@@ -149,7 +207,7 @@ int main() {
 			}
 			break;
 		}
-		case '5':
+		case '6':
 		{
 			int c = find();
 			if (c > -1) {
@@ -157,7 +215,7 @@ int main() {
 			}
 			break;
 		}
-		case '6':
+		case '7':
 		{
 			ofstream out;
 			out.open("sitename.txt");
@@ -177,13 +235,27 @@ int main() {
 
 			
 		}
-		case '7':
+
+		case '8':
+		{
+			ifstream in;
+			in.open("sitename.txt");
+			for (int i = 0; i < 50; i++) {
+				getline(in, sitename[i]);
+				getline(in, number[i]);
+				getline(in, email[i]);
+				getline(in, password[i]);
+				k++;
+			}
+			in.close();
+			break;
+		}
+		case '9':
 		{
 			continue;
 			break;
 		}
 		}
-
 	} while (op != '7');
 
 	return 0;
