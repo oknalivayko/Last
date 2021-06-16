cf25e9b4e33f4e36c9960b204877dcb55ab35a83
oknalivayko <76043317+oknalivayko@users.noreply.github.com>
2021-06-16 18:20:21 +0300

    UnitTests

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+void delSite(int c);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert()
+		}
+	};
+}
